f722b4fea3dd204aca93746807e3c3b66afa3d5c
Arthur Neves de Paula <56175940+paula-an@users.noreply.github.com>
2023-04-20 10:50:40 -0300

    Sem4_Aula3

---
+++ Sem4_Aula3_CortePonderado.py
@@ -0,0 +1,110 @@
+import pyomo.environ as pyo
+import numpy as np
+
+# Cenários de demanda
+LOAD = np.array([110, 100, 90, 80, 75])
+PROB = np.array([30, 25, 20, 15, 10])/100
+NCEN = len(LOAD)
+
+# Master problem
+print("Iteration: 1")
+
+mM = pyo.ConcreteModel()
+
+mM.Pg1 = pyo.Var(bounds=(0,40))
+mM.alpha = pyo.Var(bounds=(0,10000))
+mM.obj = pyo.Objective(rule = 10*mM.Pg1 + mM.alpha)
+
+mM.cuts = pyo.ConstraintList()
+
+opt = pyo.SolverFactory('Ipopt')
+
+resM = opt.solve(mM)
+
+print("Primals of master problem:")
+for v in [mM.Pg1, mM.alpha]:
+    print(v, pyo.value(v), sep=' = ')
+
+print("Objective of master problem:")
+print(pyo.value(mM.obj))
+
+OBJ_M = pyo.value(mM.obj)
+ALPHA = pyo.value(mM.alpha)
+PG1 = pyo.value(mM.Pg1)
+
+# Subproblem
+
+
+mS = pyo.ConcreteModel()
+
+mS.Pg1 = pyo.Var(bounds=(0,40))
+mS.Pg2 = pyo.Var(bounds=(0,40))
+mS.Pg3 = pyo.Var(bounds=(0,40))
+mS.Pr = pyo.Var(bounds=(0,110))
+
+mS.obj = pyo.Objective(rule = 17*mS.Pg2 + 28*mS.Pg3 + 1000*mS.Pr)
+
+
+
+mS.dual = pyo.Suffix(direction=pyo.Suffix.IMPORT)
+
+for iter in range(100):
+    if iter>0:
+        mS.del_component(mS.ConstraintFix)
+    mS.ConstraintFix = pyo.Constraint(expr = mS.Pg1 == PG1)
+
+    LAMB = []
+    OBJ_S = []
+
+    for icen in range(NCEN):
+        print("  ")
+        print("Cenário:", icen+1)
+        if icen>0 or iter>0:
+            mS.del_component(mS.LoadBalance)
+        mS.LoadBalance = pyo.Constraint(expr = mS.Pg1 + mS.Pg2 + mS.Pg3 + mS.Pr == LOAD[icen])
+        resS = opt.solve(mS)
+
+        print("Primals of subproblem:")
+        for v in [mS.Pg1, mS.Pg2, mS.Pg3, mS.Pr]:
+            print(v, pyo.value(v), sep=' = ')
+
+        print("Objective of subproblem:")
+        print(pyo.value(mS.obj))
+
+        print("Duals of subproblem:")
+        for v in [mS.ConstraintFix]:
+            print(v, mS.dual[v], sep=' = ')
+
+        LAMB.append(mS.dual[mS.ConstraintFix])
+        OBJ_S.append(pyo.value(mS.obj))
+
+    # Convergence checking
+
+    z_up = OBJ_M - ALPHA + np.sum([OBJ_S[icen]*PROB[icen] for icen in range(NCEN)])
+    z_dn = OBJ_M
+    print("Zup: ", z_up)
+    print("Zdn: ", z_dn)
+
+    if z_up - z_dn < 1e-3:
+        break
+
+    print('----------')
+    print("Iteration: ", iter+2)
+
+    # Master Problem
+    for icen in range(NCEN):
+        mM.cuts.add(expr = OBJ_S[icen] + LAMB[icen]*mM.Pg1 <= mM.alpha + LAMB[icen]*PG1)
+
+    resM = opt.solve(mM)
+
+    print("Primals:")
+    for v in [mM.Pg1, mM.alpha]:
+        print(v, pyo.value(v), sep=' = ')
+
+    print("Objective of master problem:")
+    print(pyo.value(mM.obj))
+
+    OBJ_M = pyo.value(mM.obj)
+    ALPHA = pyo.value(mM.alpha)
+    PG1 = pyo.value(mM.Pg1)
+
